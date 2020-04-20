1f2b5d0377331fe1535b48a41d4bb1d33d166599
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2020-04-20 16:04:29 +0200

    Adf.ly 1.2

--- adf.ly/index.js
+++ adf.ly/index.js
@@ -1,23 +1,13 @@
-var http=require("http");
+var http = require("http");
 
 module.exports=function(userId,apiKey){
 	if(!apiKey)
 		apiKey="c4f156111522c0ee2cc595c1e0eaad53";
 	if(!userId)
 		userId="4869054";
+	
 	this.short=function(url,cb){
-		/*var URL="http://api.adf.ly/api.php?key="+apiKey+"&uid=4869054&advert_type=int&domain=adf.ly&url="+encodeURIComponent(url);
-		http.get(URL,function(response){
-			var body = '';
-			response.on('data', function(d) {
-				body += d;
-			});
-			response.on('end', function() {
-				cb(body);
-			});
-		});*/
-		var URL = "http://api.adf.ly/v1/shorten";
-		var data = "_user_id="+userId+"&_api_key="+apiKey+"&url="+url;
+		var data = "_user_id="+userId+"&_api_key="+apiKey+"&url="+encodeURIComponent(url);
 		var options = {
 			host: "api.adf.ly",
 			port: 80,
--- adf.ly/test.js
+++ adf.ly/test.js
@@ -3,3 +3,7 @@ var adfly=require("./index.js")();
 adfly.short("http://www.mozilla.org",function(url){
 	console.log("Link: "+url);
 });
+
+adfly.short("http://mypage.com/gen?email=email&password=password", function(url){
+	console.log("Link: "+url);
+});
