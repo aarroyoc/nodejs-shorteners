793c3df6a27613c09a144968a6f4404f8391afe0
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2020-04-20 16:31:41 +0200

    Sh.st 1.2.0

--- sh.st/index.js
+++ sh.st/index.js
@@ -1,20 +1,37 @@
-var request=require("request");
+const https = require("https");
+const qs = require("querystring");
 
 module.exports=function(apiKey){
 	if(!apiKey)
 		apiKey="cc80433e0a07cff9b538b28eb8c6d814";
 	this.short=function(url,cb){
-		var URL="https://api.shorte.st/v1/data/url"
-		request.put(URL,{
+		const data = {
+			"urlToShorten": url
+		};
+		const putData = qs.stringify(data);
+		const options = {
+			hostname: "api.shorte.st",
+			path: "/v1/data/url",
+			method: "PUT",
 			headers: {
-				"public-api-token" : apiKey
-			},
-			form: {
-				"urlToShorten": url
+				"Content-Type": "application/x-www-form-urlencoded",
+				"Content-Length": Buffer.byteLength(putData),
+				"public-api-token": apiKey
 			}
-		},function(error,response,body){
-			cb(JSON.parse(body).shortenedUrl);
+		};
+
+		var req = https.request(options, function(response){
+			response.setEncoding('utf8');
+			var body = '';
+			response.on('data', function(d) {
+				body += d;
+			});
+			response.on('end', function() {
+				cb(JSON.parse(body).shortenedUrl);
+			});
 		});
+		req.write(putData);
+		req.end();
 	}
 	return this;
 }
